2efc3fc3b24f8ce3ee7345ef44a43af07d34932f
KULLEP <malyukow.maksim@mail.ru>
2020-02-26 22:02:09 +0200

    3_commit

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-var request = require('request');
-var iconv  = require('iconv-lite');
+ var needle = require('needle');
+ var url = 'https://aliexpress.ru/home.htm';
 
-var opt = {
-    url: 'http://www.ferra.ru/ru/techlife/news/',
-    encoding: null
-}
 
-request(opt, function (err, res, body) {
-    if (err) throw err;
-    console.log(iconv.decode(body, 'win1251'));
-    console.log(res.statusCode);
-});
+ needle.get(url, () => {
+ if (err) throw(err);
+
+ console.log("Залупная залупа");
+ });
